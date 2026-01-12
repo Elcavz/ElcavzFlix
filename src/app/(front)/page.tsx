3c14113afc78eed581e5811870a36b5fddbd3bf3
Elcavz <89607443+Elcavz@users.noreply.github.com>
2026-01-12 15:34:58 +0800

    Update page.tsx

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -13,16 +13,16 @@ export default function Index() {
         aria-labelledby="hero-heading"
         className="container mx-auto flex flex-col items-center justify-center gap-4 pb-8 pt-28 text-center md:pb-12 lg:py-32"
       >
-        <Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
+        <Link href={siteConfig.links.facebook} target="_blank" rel="noreferrer">
           <Badge
             aria-hidden="true"
             className="rounded-md px-3.5 py-1.5"
             variant="secondary"
           >
-            <Icons.twitter className="mr-2 h-3.5 w-3.5" />
-            Follow along on Twitter
+            <Icons.facebook className="mr-2 h-3.5 w-3.5" />
+            Follow along on Facebook
           </Badge>
-          <span className="sr-only">Twitter</span>
+          <span className="sr-only">Facebook</span>
         </Link>
         <h1 className="max-w-screen-lg text-center font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
           {siteConfig.name} - {siteConfig.slogan}

